e7c3b57e2d9d3b549583baa869ce88b940bf8000
Lorenzo Paoria <82080452+lorenzopaoria@users.noreply.github.com>
2026-02-09 13:56:27 +0100

    Run winfetch only on first startup
    Modify winfetch execution to run only on first launch.

--- profile.ps1
+++ profile.ps1
@@ -3,8 +3,14 @@ oh-my-posh init pwsh --config "$HOME\oh-my-posh\robbyrussell.omp.json" | Invoke-
 # Modulo per icone nel terminale
 Import-Module Terminal-Icons
 
-# neofetch all'avvio
-winfetch
+# winfetch solo al primo avvio
+$flag = "$env:USERPROFILE\.winfetch_ran"
+
+if (-Not (Test-Path $flag)) {
+    winfetch
+    New-Item -ItemType File -Path $flag | Out-Null
+}
+
 
 # Alias personalizzati
 Set-Alias ll 'Get-ChildItem -Force'
